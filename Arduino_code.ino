1e8aeff50c1995a76cf48a1c35f0a3b879526119
PAAGEO <georgekwofie2324@gmail.com>
2025-07-17 21:34:27 +0000

    Add files via upload

---
+++ Arduino_code.ino
@@ -0,0 +1,12 @@
+const int potPin = 12;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int potValue = analogRead(potPin);
+  Serial.print("Potentiometer Value: ");
+  Serial.println(potValue);
+  delay(200);
+}
